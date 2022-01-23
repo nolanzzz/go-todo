ff755d56a103cc85cf9dfa352e44f1df4d607bef
nolanzzz <nolanwork131@gmail.com>
2022-01-22 22:53:44 -0500

    add response wrapper

---
+++ common/response/response.go
@@ -0,0 +1,22 @@
+package response
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func respJson(c *gin.Context, httpCode int, msg string, data interface{}) {
+	c.JSON(httpCode, gin.H{
+		"status": httpCode,
+		"data":   data,
+		"msg":    msg,
+	})
+}
+
+func Success(c *gin.Context, msg string, data interface{}) {
+	respJson(c, http.StatusOK, msg, data)
+}
+
+func Fail(c *gin.Context, msg string, data interface{}) {
+	respJson(c, http.StatusBadRequest, msg, data)
+}
