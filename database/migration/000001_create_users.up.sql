4e861fad2efd004c45e48ee2b044baae2c095339
nolanzzz <nolanwork131@gmail.com>
2022-02-01 18:32:40 -0500

    add fk user_id to todos

--- database/migration/000001_create_users.up.sql
+++ database/migration/000001_create_users.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `users` (
-                         `id` int unsigned NOT NULL AUTO_INCREMENT,
-                         `username` varchar(255) DEFAULT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `created_at` datetime DEFAULT NULL,
-                         `updated_at` datetime DEFAULT NULL,
-                         `deleted_at` datetime DEFAULT NULL,
-                         PRIMARY KEY (`id`),
-                         KEY `idx_users_deleted_at` (`deleted_at`)
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) DEFAULT NULL,
+    `password` varchar(255) NOT NULL,
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+    `deleted_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_users_deleted_at` (`deleted_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- database/migration/000002_create_todos.up.sql
+++ database/migration/000002_create_todos.up.sql
@@ -4,10 +4,12 @@ CREATE TABLE `todos` (
     `description` varchar(255) DEFAULT NULL,
     `completed` int DEFAULT 0,
     `time_spent` int DEFAULT NULL COMMENT 'Total minutes spent',
+    `user_id` int unsigned NOT NULL,
     `created_at` datetime DEFAULT NULL,
     `updated_at` datetime DEFAULT NULL,
     `deleted_at` datetime DEFAULT NULL,
     PRIMARY KEY (`id`),
-    key `idx_todos_deleted_at` (`deleted_at`)
+    CONSTRAINT `todos_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    KEY `idx_todos_deleted_at` (`deleted_at`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
