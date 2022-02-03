a74281c1df790e9771618030bcda617d34b7149a
nolanzzz <nolanwork131@gmail.com>
2022-02-02 22:37:44 -0500

    add logger to all error handlers

--- controller/todo_controller.go
+++ controller/todo_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"todo/common/response"
 	"todo/model"
 	"todo/service/todo_service"
@@ -24,6 +25,7 @@ func (t *TodoController) Create(c *gin.Context) {
 	_ = c.ShouldBind(&todo)
 	todo.UserID = c.GetUint("user_id")
 	if err := todo_service.TodoServiceApp.Create(todo); err != nil {
+		log.Println(err.Error())
 		response.FailWithMessage(c, "todo create failed")
 	} else {
 		response.OkWithMessage(c, "todo create succeed")
@@ -42,6 +44,7 @@ func (t *TodoController) Update(c *gin.Context) {
 	var todo model.Todo
 	_ = c.ShouldBind(&todo)
 	if err := todo_service.TodoServiceApp.Update(todo); err != nil {
+		log.Println(err.Error())
 		response.FailWithMessage(c, "todo update failed")
 	} else {
 		response.OkWithMessage(c, "todo update succeed")
@@ -57,6 +60,7 @@ func (t *TodoController) Update(c *gin.Context) {
 func (t *TodoController) GetAll(c *gin.Context) {
 	todos, err := todo_service.TodoServiceApp.GetAll()
 	if err != nil {
+		log.Println(err.Error())
 		response.FailWithMessage(c, "todo get all failed")
 		return
 	}
@@ -73,6 +77,7 @@ func (t *TodoController) GetUserAll(c *gin.Context) {
 	userID := c.Param("userID")
 	items, err := todo_service.TodoServiceApp.GetUserAll(userID)
 	if err != nil {
+		log.Println(err.Error())
 		response.FailWithMessage(c, "todo get user all failed")
 		return
 	}
@@ -89,6 +94,7 @@ func (t *TodoController) Get(c *gin.Context) {
 	id := c.Param("id")
 	item, err := todo_service.TodoServiceApp.Get(id)
 	if err != nil {
+		log.Println(err.Error())
 		response.FailWithMessage(c, "todo get failed")
 		return
 	}
--- middleware/auth.go
+++ middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"strings"
 	"todo/common/jwt_helper"
 	"todo/common/response"
@@ -13,25 +14,30 @@ func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		header := c.GetHeader("Authorization")
 		if header == "" {
+			log.Println("empty header")
 			response.Unauthorized(c)
 			c.Abort()
 			return
 		}
 		split := strings.Split(header, " ")
 		if split[0] != "Bearer" && split[1] == "" {
+			log.Println("split[0]: ", split[0])
+			log.Println("split[1]: ", split[1])
 			response.Unauthorized(c)
 			c.Abort()
 			return
 		}
 		decode, err := jwt_helper.Decode(split[1])
 		if err != nil {
+			log.Println(err.Error())
 			response.Unauthorized(c)
 			c.Abort()
 			return
 		}
-		user := &model.User{}
-		err = global.DB.Find(user, "id = ?", decode.Wid).Error
+		var user model.User
+		err = global.DB.Find(&user, "id = ?", decode.Wid).Error
 		if err != nil || user.ID == 0 {
+			log.Println("id: ", decode.Wid, " ", err.Error())
 			response.NotFound(c)
 			c.Abort()
 			return
