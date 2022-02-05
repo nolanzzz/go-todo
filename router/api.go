d53563de9e04a5cd395c4e5d78b8150cfea249ce
nolanzzz <nolanwork131@gmail.com>
2022-02-04 23:20:01 -0500

    clean up router

--- router/api.go
+++ router/api.go
@@ -4,37 +4,35 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
-	"todo/controller"
+	api "todo/api/v1"
 	_ "todo/docs"
 	"todo/middleware"
 )
-
-type Router struct{}
 
 func InitApiRouter() *gin.Engine {
 	router := gin.Default()
 	// Register Swagger handler
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	v1 := router.Group("/api/v1")
-	TodoGroup := v1.Group("/todo")
+	TodoGroup := router.Group("/api/v1/todo")
 	{
-		TodoGroup.GET("/:id", controller.Todo.Get)
-		TodoGroup.GET("/", controller.Todo.GetAll)
-		TodoGroup.GET("/by/:userID", controller.Todo.GetUserAll)
-	}
-	TodoAuthGroup := TodoGroup.Use(middleware.Auth()) // Only authorized users can make changes
-	{
-		TodoAuthGroup.POST("/", controller.Todo.Create)
-		TodoAuthGroup.PUT("/", controller.Todo.Update)
-		TodoAuthGroup.PUT("/done/:id", controller.Todo.Done)
-		TodoAuthGroup.PUT("/undone/:id", controller.Todo.Undone)
+		TodoGroup.GET("/:id", api.Todo.Get)
+		TodoGroup.GET("/", api.Todo.GetAll)
+		TodoGroup.GET("/by/:userID", api.Todo.GetUserAll)
+
+		TodoAuthGroup := TodoGroup.Use(middleware.Auth()) // Only authorized users can make changes
+		{
+			TodoAuthGroup.POST("/", api.Todo.Create)
+			TodoAuthGroup.PUT("/", api.Todo.Update)
+			TodoAuthGroup.PUT("/done/:id", api.Todo.Done)
+			TodoAuthGroup.PUT("/undone/:id", api.Todo.Undone)
+		}
 	}
 
-	UserGroup := v1.Group("/users")
+	UserGroup := router.Group("/api/v1/users")
 	{
-		UserGroup.POST("/register", controller.User.Register) // Register
-		UserGroup.POST("/login", controller.User.Login)       // Login
+		UserGroup.POST("/register", api.User.Register) // Register
+		UserGroup.POST("/login", api.User.Login)       // Login
 	}
 
 	return router
