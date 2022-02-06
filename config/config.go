2c97bf633c6cbb002a8b2fb6658697eea0db02e5
nolanzzz <nolanwork131@gmail.com>
2022-02-06 16:28:40 -0500

    add migrate option to database config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,10 @@
 package config
 
 type Config struct {
-	JWT    JWT    `mapstructure:"jwt"`
-	Zap    Zap    `mapstructure:"zap"`
-	Mysql  Mysql  `mapstructure:"mysql"`
-	Redis  Redis  `mapstructure:"redis"`
-	System System `mapstructure:"system"`
+	JWT      JWT      `mapstructure:"jwt"`
+	Zap      Zap      `mapstructure:"zap"`
+	Mysql    Mysql    `mapstructure:"mysql"`
+	Redis    Redis    `mapstructure:"redis"`
+	System   System   `mapstructure:"system"`
+	Database Database `mapstructure:"database"`
 }
