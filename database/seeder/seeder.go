77d2ff5afb7611fcc01d15c8d007e32fa72dc58f
nolanzzz <nolanwork131@gmail.com>
2022-02-06 16:29:11 -0500

    create seeder interface

---
+++ database/seeder/seeder.go
@@ -0,0 +1,19 @@
+package seeder
+
+import "github.com/jinzhu/gorm"
+
+type Seeder interface {
+	TableName() string
+	Seed(*gorm.DB) error
+}
+
+func RunSeeders(db *gorm.DB) {
+	seeders := []Seeder{
+		Users,
+	}
+	for _, seeder := range seeders {
+		if err := seeder.Seed(db); err != nil {
+			panic("seeder failed: " + seeder.TableName())
+		}
+	}
+}
